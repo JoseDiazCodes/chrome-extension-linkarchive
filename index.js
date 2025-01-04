2b5396969514dfe93e664d98a75dcd7c6f89bd4b
JoseDiazCodes <josediazjr.a@gmail.com>
2025-01-04 00:41:27 -0500

    added features focusing on accessibility and styling

--- index.js
+++ index.js
@@ -3,45 +3,86 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 const tabBtn = document.getElementById("tab-btn")
 
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
+function isValidURL(string) {
+	const urlPattern = /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-./?%&=]*)?$/
+	return urlPattern.test(string)
 }
 
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
+if (leadsFromLocalStorage) {
+	myLeads = leadsFromLocalStorage
+	render(myLeads)
+}
+
+tabBtn.addEventListener("click", function () {
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		myLeads.push(tabs[0].url)
+		localStorage.setItem("myLeads", JSON.stringify(myLeads))
+		render(myLeads)
+	})
 })
 
 function render(leads) {
-    let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
-        listItems += `
+	let listItems = ""
+	for (let i = 0; i < leads.length; i++) {
+		listItems += `
             <li>
                 <a target='_blank' href='${leads[i]}'>
                     ${leads[i]}
                 </a>
             </li>
         `
-    }
-    ulEl.innerHTML = listItems
+	}
+	ulEl.innerHTML = listItems
 }
 
-deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
+const handleDelete = () => {
+	localStorage.clear()
+	myLeads = []
+	render(myLeads)
+}
+
+deleteBtn.addEventListener("dblclick", handleDelete)
+
+const handleInput = () => {
+	if (!isValidURL(inputEl.value)) {
+		alert("Please enter a valid URL")
+		return
+	}
+
+	myLeads.push(inputEl.value)
+	inputEl.value = ""
+
+	localStorage.setItem("myLeads", JSON.stringify(myLeads))
+	render(myLeads)
+}
+
+inputEl.addEventListener("keydown", (event) => {
+	if (event.key === "Enter") {
+		event.preventDefault()
+		handleInput()
+	}
 })
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
-})
+inputBtn.addEventListener("click", handleInput)
+
+// inputBtn.addEventListener("click", function () {
+// 	if (!isValidURL(inputEl.value)) {
+// 		alert("Please enter a valid URL")
+// 		return
+// 	}
+
+// 	myLeads.push(inputEl.value)
+// 	inputEl.value = ""
+
+// 	localStorage.setItem("myLeads", JSON.stringify(myLeads))
+// 	render(myLeads)
+// })
+
+// deleteBtn.addEventListener("dblclick", function () {
+// 	localStorage.clear()
+// 	myLeads = []
+// 	render(myLeads)
+// })
